9a1c35f69d5a20047ea6e823b60d51059ac8fb4d
Tejendra Khatri <khatri.tejen@gmail.com>
2020-06-16 23:04:45 -0500

    Update two_threads.c

--- two_threads.c
+++ two_threads.c
@@ -18,7 +18,7 @@ void *factorial_thread(void *arg)
         factResult *= i;
     printf("The factorial of %d is : %d\n",*num,factResult);
 }
-
+ 
 //Gauss-Legendre method
 double piValue(int numIterations)
 {
